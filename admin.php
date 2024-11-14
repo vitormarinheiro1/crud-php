6749757f42475858297876be9c696d3d27d83604
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-14 14:10:53 -0300

    feat: add cadastrando produto

--- admin.php
+++ admin.php
@@ -63,7 +63,7 @@ $produtos = $produtoRepositorio->buscarTodos();
       <?php endforeach; ?>
       </tbody>
     </table>
-  <a class="botao-cadastrar" href="cadastrar-produto.html">Cadastrar produto</a>
+  <a class="botao-cadastrar" href="cadastrar-produto.php">Cadastrar produto</a>
   <form action="#" method="post">
     <input type="submit" class="botao-cadastrar" value="Baixar Relatório"/>
   </form>

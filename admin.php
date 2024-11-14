2500298e054517907a180369acf22b4b7db6e497
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-14 16:18:02 -0300

    feat: add edição

--- admin.php
+++ admin.php
@@ -52,7 +52,7 @@ $produtos = $produtoRepositorio->buscarTodos();
         <td><?= $produto->getTipo() ?></td>
         <td><?= $produto->getDescricao() ?></td>
         <td><?= $produto->getPrecoFormatado() ?></td>
-        <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
+        <td><a class="botao-editar" href="editar-produto.php?id=<?= $produto->getId() ?>">Editar</a></td>
         <td>
           <form action="excluir-produto.php" method="post">
             <input type="hidden" name="id" value="<?= $produto->getId()?>">
--- src/Repository/ProdutoRepositorio.php
+++ src/Repository/ProdutoRepositorio.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class ProdutoRepositorio
 {
     private PDO $pdo;
@@ -8,7 +7,6 @@ class ProdutoRepositorio
     /**
      * @param PDO $pdo
      */
-
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
@@ -16,8 +14,7 @@ class ProdutoRepositorio
 
     private function formarObjeto($dados)
     {
-        return new Produto(
-            $dados['id'],
+        return new Produto($dados['id'],
             $dados['tipo'],
             $dados['nome'],
             $dados['descricao'],
@@ -32,24 +29,23 @@ class ProdutoRepositorio
         $statement = $this->pdo->query($sql1);
         $produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-        $dadosCafe = array_map(function ($cafe) {
+        $dadosCafe = array_map(function ($cafe){
             return $this->formarObjeto($cafe);
-        }, $produtosCafe);
+        },$produtosCafe);
 
         return $dadosCafe;
     }
-
     public function opcoesAlmoco(): array
     {
         $sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço' ORDER BY preco";
         $statement = $this->pdo->query($sql2);
         $produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-        $dadosAlmoco = array_map(function ($almoco) {
+        $dadosAlmoco = array_map(function ($almoco){
             return $this->formarObjeto($almoco);
-        }, $produtosAlmoco);
+        },$produtosAlmoco);
 
-        return $dadosAlmoco;
+        return  $dadosAlmoco;
     }
 
     public function buscarTodos()
@@ -58,9 +54,9 @@ class ProdutoRepositorio
         $statement = $this->pdo->query($sql);
         $dados = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-        $todosOsDados = array_map(function ($produto) {
+        $todosOsDados = array_map(function ($produto){
             return $this->formarObjeto($produto);
-        }, $dados);
+        },$dados);
 
         return $todosOsDados;
     }
@@ -69,8 +65,9 @@ class ProdutoRepositorio
     {
         $sql = "DELETE FROM produtos WHERE id = ?";
         $statement = $this->pdo->prepare($sql);
-        $statement->bindValue(1, $id);
+        $statement->bindValue(1,$id);
         $statement->execute();
+
     }
 
     public function salvar(Produto $produto)
@@ -80,8 +77,35 @@ class ProdutoRepositorio
         $statement->bindValue(1, $produto->getTipo());
         $statement->bindValue(2, $produto->getNome());
         $statement->bindValue(3, $produto->getDescricao());
-        $statement->bindValue(4, $produto->getPreco());
+        $statement->bindValue(4,$produto->getPreco());
         $statement->bindValue(5, $produto->getImagem());
         $statement->execute();
     }
+
+    public function buscar(int $id)
+    {
+        $sql = "SELECT * FROM produtos WHERE id = ?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $id);
+        $statement->execute();
+
+        $dados = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $this->formarObjeto($dados);
+    }
+
+    public function atualizar(Produto $produto)
+    {
+        $sql = "UPDATE produtos SET tipo = ?, nome = ?, descricao = ?, preco = ?, imagem = ? WHERE id = ?";
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $produto->getTipo());
+        $statement->bindValue(2, $produto->getNome());
+        $statement->bindValue(3, $produto->getDescricao());
+        $statement->bindValue(4,$produto->getPreco());
+        $statement->bindValue(5, $produto->getImagem());
+        $statement->bindValue(6, $produto->getId());
+        $statement->execute();
+    }
+
+
 }
